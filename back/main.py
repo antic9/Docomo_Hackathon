6c2c94367a2e0e138421cdb1b6644c335380b274
Naoya Ikushima <alexnaoya@gmail.com>
2021-09-14 15:37:24 +0900

    connect database

--- back/main.py
+++ back/main.py
@@ -1,39 +1,35 @@
 from flask import Flask, redirect, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+import pymysql
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Kyotsuu5@18.222.210.94/Docomo_Hackathon?charset=utf8'
+# db = SQLAlchemy(app)
+# engine = create_engine('mysql+pymysql://root:Kyotsuu5@18.222.210.94/Docomo_Hackathon?charset=utf8')
+def getConnection():
+    connection = pymysql.connect(
+    host="localhost",
+    db="hackathon",
+    user="root",
+    password="Kyotsuu5",
+    charset="utf8",
+    cursorclass=pymysql.cursors.DictCursor
+)
+    return connection
 
-dns = {
-    'user': 'root',
-    'host': '18.222.210.94', # 各自設定
-    'port':'3306',
-    'password': 'Kyotsuu5', # 各自設定
-    'database': 'Docomo_Hackathon' # 各自設定
-}
-
-
-class DATABASENAME(db.Model):
-  __tablename__ = 'DATABASENAME'
+# class DATABASENAME(db.Model):
+#   __tablename__ = 'DATABASENAME'
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-  if request.method == 'GET':
-    return render_template('index_test_flask_base.html')
-  else:
-    return redirect('/')
+    print("connected")
+    connection = getConnection()
+    message = "Successfully Connected to SQL"
 
-@app.route('/login')
-def login():
-  return redirect('/')
+    sql = "SELECT * FROM user"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    user = cursor.fetchall()
 
-@app.route('/mypage')
-def mypage():
-  return redirect('/')
-
-@app.route('personal_page')
-def personal_page():
-  return redirect('/')
-
-
+    cursor.close()
+    connection.close()
+    return render_template('index_test_flask_base.html', message = message, users = user)
