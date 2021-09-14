092d0630ec60add5636b44f47b11fbe12a0dfa7c
kd-kojima <89893738+kd-kojima@users.noreply.github.com>
2021-09-14 11:00:04 +0900

    flask_base
    temp flask

---
+++ back/main.py
@@ -0,0 +1,30 @@
+from flask import Flask, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
+db = SQLAlchemy(app)
+
+class DATABASENAME(db.Model):
+  __tablename__ = 'DATABASENAME'
+
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+  if request.method == 'GET':
+    return render_template('index_test_flask_base.html')
+  else:
+    return redirect('/')
+
+@app.route('/login')
+def login():
+  return redirect('/')
+
+@app.route('/mypage')
+def mypage():
+  return redirect('/')
+
+@app.route('personal_page')
+def personal_page():
+  return redirect('/')
+
+
